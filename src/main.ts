e60c5c6b917302abff5705ee47236f33b029a744
DJMarshallSantos <marceloaquinodossantos@gmail.com>
2022-06-09 08:26:32 -0400

    auths

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,12 @@ async function bootstrap() {
     .setDescription('Aplicação para gestão de mesas para pizzaria')
     .setVersion('1.0.0')
     .addTag('status')
+    .addTag('auth')
     .addTag('table')
     .addTag('product')
     .addTag('user')
     .addTag('order')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
